41e2257663632e4967b532749ade0e750bcab5ee
Rob Azinger <azinger3@gmail.com>
2016-10-10 16:22:39 -0400

    updated transaction insert

--- query/TransactionInsert.sql
+++ query/TransactionInsert.sql
@@ -1,6 +1,9 @@
-DELIMITER $$
-CREATE PROCEDURE `TransactionInsert`(TransactionTypeID INT, TransactionNumber VARCHAR(100), TransactionDT DATETIME, BudgetID INT, BudgetCategoryID INT, Amount DECIMAL(10 ,4), Description VARCHAR(1000), Note VARCHAR(1000))
+CREATE PROCEDURE `TransactionInsert`(TransactionTypeID INT, TransactionNumber VARCHAR(100), TransactionDT DATETIME, BudgetCategoryID INT, Amount DECIMAL(10 ,4), Description VARCHAR(1000), Note VARCHAR(1000))
 BEGIN
+	DECLARE BudgetID INT;
+    
+    SET BudgetID = (SELECT Budget.BudgetID FROM Budget Budget WHERE MONTH(Budget.BudgetMonth) = MONTH(TransactionDT));
+
 	INSERT INTO Transaction
     (
 		TransactionTypeID
@@ -25,5 +28,4 @@ BEGIN
             ,NOW()
             ,'User'
     ;
-END$$
-DELIMITER ;
+END
