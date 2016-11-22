abdcaced23bccb16cfdc3ba99ffd9a70e9f2d68f
Rob Azinger <azinger3@gmail.com>
2016-11-21 23:31:04 -0500

    cors support

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ require 'vendor/autoload.php';
 
 $app = new Slim\App();
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 require 'route/index.route.php';
 require 'route/budget.route.php';
 require 'route/todo.route.php';
