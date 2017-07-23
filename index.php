6b4be791a18a69cfcaae24578f50ec6b8fa52859
Rob Azinger <azinger3@gmail.com>
2017-07-23 14:53:51 -0400

    fix

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $app->add(function ($req, $res, $next) {
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
 require 'route/index.route.php';
