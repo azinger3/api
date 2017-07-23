35b2ef742a9bbf4955fe07470d3f54c4c3d5c85d
Rob Azinger <azinger3@gmail.com>
2017-07-23 14:56:45 -0400

    try it

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $app->add(function ($req, $res, $next) {
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 });
 
 require 'route/index.route.php';
